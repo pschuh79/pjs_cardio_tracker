55501a0ac85bfb68a93e1374cf409d48278830d6
pschuh79 <pschuh79@gmail.com>
2022-09-26 20:39:54 -0500

    initial

---
+++ src/components/SessionEntry/SessionEntry.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './sessionEntry.css';
+
+function SessionEntry() {
+  return (
+    <div className='session'>SessionEntry</div>
+  )
+}
+
+export default SessionEntry
